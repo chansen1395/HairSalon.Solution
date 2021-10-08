65eb3b9481e3033959007b4fa61fb2c9a6ca8ad9
Connor Hansen <chansen13@georgefox.edu>
2021-10-08 11:16:45 -0700

    WIP troubleshooting.

--- HairSalon/Models/SalonContext.cs
+++ HairSalon/Models/SalonContext.cs
@@ -4,7 +4,7 @@ namespace Salon.Models
 {
   public class SalonContext : DbContext
   {
-    public DbSet<Stylist> Stylists { get; set; }
+    public DbSet<Stylist> Hair_Stylists { get; set; }
     public DbSet<Client> Clients { get; set; }
 
     public SalonContext(DbContextOptions options) : base(options) { }
